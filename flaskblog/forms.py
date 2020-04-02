94e13fd08fce16403cfc2cac650bd463d8389b03
jaijhala <jaiveer18@gmail.com>
2020-04-02 15:48:56 -0700

    Removed that print command in forms.py

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -54,5 +54,4 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
-print("tera hoke")
 
